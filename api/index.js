40b2c552d0388c5be6f666365367a73898a0e615
Kasifmansuri7 <kasif.mansuri.7@gmail.com>
2023-11-29 17:40:38 +0530

    render deployment

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use(cors());
 
 //----------------------------Deployment--------------------------
 
-const __dirname1 = path.resolve(__dirname, "..");
+const __dirname1 = path.resolve(__dirname);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/client/dist")));
