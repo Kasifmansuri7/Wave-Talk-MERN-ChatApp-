f7f0b0bc7499d4aa9f2a934d3f4428169925c953
Kasifmansuri7 <kasif.mansuri.7@gmail.com>
2023-11-29 16:06:27 +0530

    deployment

--- client/src/Components/SingleChat.jsx
+++ client/src/Components/SingleChat.jsx
@@ -20,7 +20,7 @@ import io from "socket.io-client";
 import Lottie from "react-lottie";
 import animationData from "../animations/typing.json";
 
-const ENDPOINT = "http://localhost:3000";
+const ENDPOINT = import.meta.env.VITE_BACKEND_URL;
 let socket, selectedChatCompare;
 
 const defaultOptions = {
