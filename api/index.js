4032bf7bcc718a24125d9005cd0faf556604d7ec
Kasifmansuri7 <kasif.mansuri.7@gmail.com>
2024-01-31 21:53:00 +0530

    database url bug

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 
 //DB connection
 (async () => {
-  await connectDB(process.env.MONGO_URL);
+  await connectDB(process.env.MONGO_URL+"/waveTalk");
 })();
 
 //middlewares
