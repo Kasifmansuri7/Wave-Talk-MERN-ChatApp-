b5fd80ab78e880ecee553ee3ca3a44f9b6157a9e
Kasifmansuri7 <kasif.mansuri.7@gmail.com>
2024-06-29 13:00:58 +0530

    fix: cors error

--- api/index.js
+++ api/index.js
@@ -44,7 +44,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
   },
 });
 
